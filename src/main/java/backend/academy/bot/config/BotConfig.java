aa06686ee312fa1b15195aaabd48da2a8fb387fc
Stepa <stepan_popov_2025@mail.ru>
2025-05-30 01:17:49 +0300

    feat: change

--- src/main/java/backend/academy/bot/config/BotConfig.java
+++ src/main/java/backend/academy/bot/config/BotConfig.java
@@ -3,4 +3,8 @@ package backend.academy.bot.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(String telegramToken) {}
+public record BotConfig(String telegramToken) {
+    public String telegramToken() {
+        return telegramToken.replaceAll("^\"|\"$", "");
+    }
+}
